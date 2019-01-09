32f8d3d7d83fee885c5130b58ae5c8f430e60d25
greatif <greatif@live.de>
2019-01-09 16:15:02 +0100

    Update export.php

--- pages/export.php
+++ pages/export.php
@@ -15,13 +15,13 @@ if ($yform->objparams['actions_executed']) {
 		$table_names = array("rex_kunden","rex_kunden_rechnungen");
 
 		# Export Tabellenstrukturen - .JSON
-		# $fileName = 'backup_kundendaten_rechnungen_'.date('Y-m-d_H-i-s').'.json';
+		# $fileName = 'export_kundendaten_rechnungen_'.date('Y-m-d_H-i-s').'.json';
 		# $fileContent = rex_yform_manager_table_api::exportTablesets($table_names);
 		# header('Content-Disposition: attachment; filename="' . $fileName . '"; charset=utf-8');
 		# rex_response::sendContent($fileContent, 'application/octetstream');
 		
 		# Export Tabellenstrukturen und Inhalt - .SQL
-		$fileName = 'backup_kundendaten_rechnungen_'.date('Y-m-d_H-i-s').'.sql';
+		$fileName = 'export_kundendaten_rechnungen_'.date('Y-m-d_H-i-s').'.sql';
 		$fileContent = rex_backup::exportDb($fileName, $table_names);
 		$header = 'plain/text';
 		rex_response::sendFile($fileName, $header, 'attachment');
