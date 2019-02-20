083255b2864134e3d1c7089b1b066abbacd35833
greatif <greatif@live.de>
2019-02-20 16:30:48 +0100

    Add files via upload

---
+++ pages/rechnungssteller.php
@@ -0,0 +1,216 @@
+<?php
+$content = '<div id="kundenverwaltung-wrapper">';
+
+if (rex_post('config-submit', 'boolean'))
+{
+    $this->setConfig(rex_post('config', [
+        ['kundenverwaltung_rechnungssteller', 'string'],
+        ['kundenverwaltung_strasse', 'string'],
+        ['kundenverwaltung_plz-ort', 'string'],
+		['kundenverwaltung_telefon', 'string'],
+		['kundenverwaltung_email', 'string'],
+		['kundenverwaltung_internet', 'string'],
+		['kundenverwaltung_steuernummer', 'string'],
+		['kundenverwaltung_kreditinstitut', 'string'],
+		['kundenverwaltung_bankleitzahl', 'string'],
+		['kundenverwaltung_kontonummer', 'string'],
+		['kundenverwaltung_iban', 'string'],
+		['kundenverwaltung_bic', 'string'],
+		['kundenverwaltung_logo', 'string']
+    ]));
+
+    $content .= rex_view::info('Änderung gespeichert');
+}
+
+$content .= '
+<form action="' . rex_url::currentBackendPage() . '" method="post" id="kundenverwaltung_settings">
+    <fieldset>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_rechnungssteller") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_rechnungssteller" name="config[kundenverwaltung_rechnungssteller]" placeholder="Vorname Name / Firmenname" value="';
+					$content .= $this->getConfig('kundenverwaltung_rechnungssteller');
+					$content .= '" />
+				</dd>
+        </dl>
+		
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_strasse") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_strasse" name="config[kundenverwaltung_strasse]" placeholder="Straße und Haus-Nr." value="';
+					$content .= $this->getConfig('kundenverwaltung_strasse');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_plz-ort") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_plz-ort" name="config[kundenverwaltung_plz-ort]" placeholder="Postleitzahl und Ort" value="';
+					$content .= $this->getConfig('kundenverwaltung_plz-ort');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_telefon") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_telefon" name="config[kundenverwaltung_telefon]" placeholder="Telefon-Nr." value="';
+					$content .= $this->getConfig('kundenverwaltung_telefon');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_email") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_email" name="config[kundenverwaltung_email]" placeholder="E-Mail-Adresse" value="';
+					$content .= $this->getConfig('kundenverwaltung_email');
+					$content .= '" />
+				</dd>
+        </dl>
+		
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_internet") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_internet" name="config[kundenverwaltung_internet]" placeholder="Internet-Adresse" value="';
+					$content .= $this->getConfig('kundenverwaltung_internet');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_steuernummer") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_steuernummer" name="config[kundenverwaltung_steuernummer]" placeholder="Steuer-Nr." value="';
+					$content .= $this->getConfig('kundenverwaltung_steuernummer');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_kreditinstitut") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_kreditinstitut" name="config[kundenverwaltung_kreditinstitut]" placeholder="Kreditinstitut" value="';
+					$content .= $this->getConfig('kundenverwaltung_kreditinstitut');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_bankleitzahl") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_bankleitzahl" name="config[kundenverwaltung_bankleitzahl]" placeholder="BLZ" value="';
+					$content .= $this->getConfig('kundenverwaltung_bankleitzahl');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_kontonummer") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_kontonummer" name="config[kundenverwaltung_kontonummer]" placeholder="Konto-Nr." value="';
+					$content .= $this->getConfig('kundenverwaltung_kontonummer');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_iban") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_iban" name="config[kundenverwaltung_iban]" placeholder="IBAN" value="';
+					$content .= $this->getConfig('kundenverwaltung_iban');
+					$content .= '" />
+				</dd>
+        </dl>
+
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_bic") . '</dt>
+				<dd>
+					<input class="rex-form-text form-control" type="text" id="rex-form-kundenverwaltung_bic" name="config[kundenverwaltung_bic]" placeholder="BIC" value="';
+					$content .= $this->getConfig('kundenverwaltung_bic');
+					$content .= '" />
+				</dd>
+        </dl>
+		
+        <dl class="rex-form-group form-group">
+			<dt>' . $this->i18n("kundenverwaltung_logo") . '</dt>
+				<dd>
+					<input class="form-control" type="text" name="config[kundenverwaltung_logo]" value="' . $this->getConfig('kundenverwaltung_logo') . '" id="REX_MEDIA_1" readonly="readonly"';
+					$content .= $this->getConfig('kundenverwaltung_logo');
+					$content .= '" />		
+					<span class="input-group-btn">
+						<a href="#" class="btn btn-popup" onclick="openREXMedia(1);return false;" title="Öffnen">
+							<i class="rex-icon rex-icon-open-mediapool"></i>
+						</a>
+						<a href="#" class="btn btn-popup" onclick="addREXMedia(1);return false;" title="Neu">
+							<i class="rex-icon rex-icon-add-media"></i>
+						</a>
+						<a href="#" class="btn btn-popup" onclick="deleteREXMedia(1);return false;" title="Entfernen">
+							<i class="rex-icon rex-icon-delete-media"></i>
+						</a>
+						<a href="#" class="btn btn-popup" onclick="viewREXMedia(1);return false;" title="Ansehen">
+							<i class="rex-icon rex-icon-view-media"></i>
+						</a>
+					</span>
+				</dd>
+		</dl>
+
+        <dl class="rex-form-group form-group">
+            <dd>
+                <button class="btn btn-save" type="submit" name="config-submit" value="1" title="' . $this->i18n('com_auth_config_save') . '">' . $this->i18n('kundenverwaltung_save') . '</button>
+            </dd>
+        </dl>
+
+    </fieldset>    
+</form>
+</div>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'edit');
+$fragment->setVar('title', $this->i18n('kundenverwaltung_title'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
+
+?>
+
+<style>
+
+    #kundenverwaltung_settings label 
+    {
+        padding-left: 0;
+    }
+
+    #kundenverwaltung_settings input 
+    {
+        margin-left: 0;
+        position: relative;
+    }
+
+    #kundenverwaltung_settings input[type=text]
+    {
+        margin-top: 5px;
+        width: 500px;
+    }
+
+    #kundenverwaltung_settings i 
+    {
+        color: #888888;
+    }
+
+    #kundenverwaltung_settings b
+    {
+        color: red;
+    }
+
+</style>
+
+<script>
+    var jSaveInfo = jQuery("#kundenverwaltung-wrapper .alert-info");
+
+    window.setTimeout(function()
+    {
+        jSaveInfo.slideUp();
+    }, 2000);
+</script>
